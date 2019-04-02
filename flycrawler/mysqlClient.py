8de2c24c392023b5559541458a08c172edb60753
fictio <ficito.d2k@gmail.com>
2019-04-02 18:53:12 +0800

    fix: 格式调整;#190402

--- flycrawler/mysqlClient.py
+++ flycrawler/mysqlClient.py
@@ -1,27 +1,28 @@
-#coding=utf-8
+# -*- coding: utf-8 -*-
 import pymysql
 class MySqlClient():
-	def __init__(self,host,port,user,pwd,db):
-		self.conn = None
-		self.host = host
-		self.port = port
-		self.user = user
-		self.pwd = pwd
-		self.db = db
+    def __init__(self,host,port,user,pwd,db):
+        self.conn = None
+        self.host = host
+        self.port = port
+        self.user = user
+        self.pwd = pwd
+        self.db = db
 
-	def __getConn(self):
-		if self.conn is None:
-			return pymysql.connect(host=self.host,port=self.port,user=self.user,
-				password=self.pwd,db=self.db,charset="UTF8")
-		else:
-			return self.conn
+    def __getConn(self):
+        if self.conn is None:
+            return pymysql.connect(host=self.host,port=self.port,user=self.user,password=self.pwd,db=self.db,charset="UTF8")
+        else:
+            return self.conn
 
-	def dosql(self,sql):
-		conn = self.__getConn()
-		cursor = conn.cursor()
+    def dosql(self,sql):
+        conn = self.__getConn()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
 
-	def dosqlBatch(self,sql,dates):
-		conn = self.__getConn()
-		cursor = conn.cursor()
-		cursor.executemany(sql,dates)
-		conn.commit()
+    def dosqlBatch(self,sql,dates):
+        conn = self.__getConn()
+        cursor = conn.cursor()
+        cursor.executemany(sql,dates)
+        conn.commit()
