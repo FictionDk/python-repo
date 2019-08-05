e0c0d9581b48324644dbe9b01fc19a61ec38fd2c
fictio <ficito.d2k@gmail.com>
2019-08-05 20:37:01 +0800

    feat: mnist tensor complecation;#190805

--- tensor-note/tensor_4_1_mnist_forward.py
+++ tensor-note/tensor_4_1_mnist_forward.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import tensorflow.compat.v1 as tf
+import tensorflow as tensor
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -10,14 +11,14 @@ LAYER1_NODE = 500
 def get_weight(shape,regularizer):
     w = tf.Variable(tf.truncated_normal(shape,stddev=0.1))
     if regularizer is not None:
-        tf.add_to_collection('losses',tf.contrib.layers.l2_regularizer(regularizer)(w))
+        tf.add_to_collection('losses',tensor.contrib.layers.l2_regularizer(regularizer)(w))
     return w
 
 def get_bias(shape):
     b = tf.Variable(tf.zeros(shape))
     return b
 
-def forword(x, regularizer):
+def forward(x, regularizer):
     w1 = get_weight([INPUT_NODE,LAYER1_NODE],regularizer)
     b1 = get_bias([LAYER1_NODE])
     y1 = tf.nn.relu(tf.matmul(x,w1) + b1)
