c500848f65470261a979df168eb21e18a4433f7e
hek <ficito.d2k@gmail.com>
2019-08-10 11:09:14 +0800

    docs: add description in codes;#190810

--- tensor-note/tensor_4_1_mnist_backward.py
+++ tensor-note/tensor_4_1_mnist_backward.py
@@ -16,28 +16,43 @@ MODEL_SAVE_PATH = "./model/"
 MODEL_NAME = "mnist_model"
 
 def backword(mnist):
+    # 给训练数据x,标签y_占位
     x = tf.placeholder(tf.float32,[None,mnist_forward.INPUT_NODE])
     y_ = tf.placeholder(tf.float32,[None,mnist_forward.OUTPUT_NODE])
+    # 使用前向传播过程,设置是否正则化,计算预测结果y
     y = mnist_forward.forward(x, REGULARIZER)
+    # 轮数计数器,不可训练
     global_step = tf.Variable(0,trainable=False)
+
+    # 设置损失函数
     ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,labels=tf.argmax(y_,1))
     cem = tf.reduce_mean(ce)
     loss = cem + tf.add_n(tf.get_collection('losses'))
+
+    # 设定指数衰减学习率
     learning_rate = tf.train.exponential_decay(
         LEARNING_RATE_BASE,
         global_step,
         mnist.train.num_examples / BATCH_SIZE,
         LEARNING_RATE_DECAY,
         staircase=True)
+
+    # 定义反向传播方法
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
+
+    # 滑动平均: 记录一段时间内模型的所有参数w和b各自的平均值,影子值,追随参数的变化而变化
+    # MOVING_AVERAGE_DECAY: 滑动平均衰减率
     ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
     ema_op = ema.apply(tf.trainable_variables())
     with tf.control_dependencies([train_step,ema_op]):
         train_op = tf.no_op(name='train')
+
     saver = tf.train.Saver()
+
     with tf.Session() as sess:
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
+
         for i in range(STEPS):
             xs,ys = mnist.train.next_batch(BATCH_SIZE)
             _,loss_value,step = sess.run([train_op,loss,global_step],feed_dict={x: xs,y_:ys})
--- tensor-note/tensor_4_1_mnist_forward.py
+++ tensor-note/tensor_4_1_mnist_forward.py
@@ -3,26 +3,42 @@ import tensorflow.compat.v1 as tf
 import tensorflow as tensor
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
+'''
+前向传播过程定义
+需要定义神经网络中的参数w和偏置b
+'''
+# 网络输入结点,(28*28图的像素个数)
 INPUT_NODE = 784
+# 输出节点,(0-9个分类)
 OUTPUT_NODE = 10
+# 隐藏层节点
 LAYER1_NODE = 500
 
+# 定义参数w
+# shape: 参数w的形状
 def get_weight(shape,regularizer):
     w = tf.Variable(tf.truncated_normal(shape,stddev=0.1))
     if regularizer is not None:
         tf.add_to_collection('losses',tensor.contrib.layers.l2_regularizer(regularizer)(w))
     return w
 
+# 获取偏置b
+# shape: 参数b的形状
 def get_bias(shape):
     b = tf.Variable(tf.zeros(shape))
     return b
 
+# 前向传播定义网络结构(预测模型)
+# regularizer: 是否需要正则化(正则化权重)
 def forward(x, regularizer):
+    # 输入层到隐藏层,w1[784,500]
     w1 = get_weight([INPUT_NODE,LAYER1_NODE],regularizer)
     b1 = get_bias([LAYER1_NODE])
+    # tf.nn.relu--激活函数,提高模型表达力
+    # 实测结果,relu函数在本次手写数字识别中优于sigmoid函数
     y1 = tf.nn.relu(tf.matmul(x,w1) + b1)
-
+    # y1 = tf.nn.sigmoid(tf.matmul(x,w1) + b1)
+    # 隐藏层到输出层,w2[500,10]
     w2 = get_weight([LAYER1_NODE,OUTPUT_NODE],regularizer)
     b2 = get_bias([OUTPUT_NODE])
     y = tf.matmul(y1,w2) + b2
