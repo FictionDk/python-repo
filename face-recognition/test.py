10bb1a67d0cf91c495e02440e1829435cac1f597
fictio <ficito.d2k@gmail.com>
2019-08-14 18:50:04 +0800

    feat: file upload;#190814'

--- face-recognition/test.py
+++ face-recognition/test.py
@@ -1,3 +1,4 @@
+import requests
 import json
 
 def test():
@@ -9,5 +10,25 @@ def test():
     print(c,"|type is",type(c))
     pass
 
+def upload_file():
+    url = "http://192.168.110.48/upload"
+    file_name = "E:\\home\\yuncloud\\Lenka - Blue Skies.mp3"
+    data = None
+    files = {"Skies.mp3" : open(file_name, "rb").read()}
+    r = requests.post(url,data,files=files)
+    path = "http://192.168.110.48/file"
+    is_ok = False
+    if r.ok :
+        rj = json.loads(r.content)
+        if rj["code"] is 200: 
+            path += rj["data"]["fileurl"]
+            is_ok = True
+        else: 
+            print("upload filed, result: %s." % str(rj))
+    else:
+        print("Connect failed,code is %s." % str(r))
+    if is_ok:
+        print(path)
 
-test()
+if __name__ == '__main__':
+    upload_file()
