a07f3114d152a3b34215c1b5e76b1824b62fa7e1
fictio <ficito.d2k@gmail.com>
2019-08-23 18:16:10 +0800

    feat: plt demo add;#190823

---
+++ tensor-note/tensor_6_plt.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plt_show0():
+    plt.plot([1,2,3,4])
+    plt.ylabel('Numbers')
+    plt.show()
+
+def plt_show1():
+    # 设置点(1,1) (2,4) (3,9) ...
+    plt.plot([1,2,3,4],[1,4,9,16],'ro')
+    # 设置x轴范围0~6 y轴0~20
+    plt.axis([0,6,0,20])
+    plt.show()
+
+def plt_show2():
+    t = np.arange(0.,5.,0.2)
+    print(t.shape)
+    print(t)
+    # 红色 -- 蓝色 s(方块) 绿色^(三角)
+    plt.plot(t,t,'r--',t,t**2,'bs',t**3,'g^')
+    plt.show()
+
+def dat_f0(t):
+    return np.exp(-t)*np.cos(2*np.pi*t)
+
+def plt_fig_show0():
+    t1 = np.arange(0.0,5.0,0.1)
+    t2 = np.arange(0.0,5.0,0.2)
+
+    plt.figure("Title")
+    plt.subplot(211)
+    plt.plot(t1,dat_f0(t1),'bo',t2,dat_f0(t2),'k')
+
+    plt.subplot(212)
+    plt.plot(t2,np.cos(2*np.pi*t2),'r--')
+
+    plt.show()
+
+def main():
+    # plt_show1()
+    # plt_show2()
+    plt_fig_show0()
+
+if __name__ == "__main__":
+    main()
