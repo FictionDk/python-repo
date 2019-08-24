7d852e78e0d3f608b1009dac47fcf1b86ebf6566
hek <ficito.d2k@gmail.com>
2019-08-24 10:54:01 +0800

    feat: plt's model demo update;#190824

--- tensor-note/tensor_6_plt.py
+++ tensor-note/tensor_6_plt.py
@@ -24,6 +24,7 @@ def plt_show2():
 
 def dat_f0(t):
     return np.exp(-t) * np.cos(2 * np.pi * t)
+
 def plt_fig_show0():
     t1 = np.arange(0.0,5.0,0.1)
     t2 = np.arange(0.0,5.0,0.2)
@@ -37,10 +38,49 @@ def plt_fig_show0():
 
     plt.show()
 
+def plt_figs_show():
+    plt.figure("fig_1")  # 编号fig_1的figure
+    plt.subplot(211)   # fig_1中的第一个子图
+    plt.plot([1,3,4],'r')
+    plt.subplot(212)  # fig_1中的第二个子图
+    plt.plot([4,5,6])
+
+    plt.figure("fig_2")  # 编号fig_2的figure
+    # 默认使用subplot(111),此时fig_2为当前figure
+    plt.plot([7,8,9],'g^',[7,8,9],'b')
+
+    plt.figure("fig_1")  # 切换当前figure
+    plt.subplot(212)  # 切换fig_1的子图
+    plt.title('Easy fig_1 4,5,6')  # 为选取子图添加标题
+
+    plt.show()
+
+def plt_grid_show():
+    mu,sigma = 100,15
+    x = mu + sigma * np.random.randn(10000)
+
+    # 绘制直方图
+    n,bins,patches = plt.hist(x,50,density=10,color='g')
+    plt.hist
+    print(n)
+    print(bins)
+    print(patches)
+
+    plt.xlabel('Smarts')
+    plt.ylabel('Probality')
+    plt.title('Histogram of IQ')
+    # 位置(60,0.025) r表示后面的字符串是纯粹字符串
+    plt.text(60,0.025,r'$\mu=100,sigma=15$')
+    plt.axis([40,160,0,0.03])
+    plt.grid(True)
+    plt.show()
+
 def main():
     # plt_show1()
     # plt_show2()
-    plt_fig_show0()
+    # plt_fig_show0()
+    # plt_figs_show()
+    plt_grid_show()
 
 if __name__ == "__main__":
     main()
