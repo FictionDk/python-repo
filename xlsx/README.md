eef33eef4b1d67d4b8fda487e1d3b8127114b999
fictio <ficito.d2k@gmail.com>
2020-01-19 17:33:47 +0800

    fix: get pics from dir and trans to other dir;#0119

--- xlsx/README.md
+++ xlsx/README.md
@@ -3,3 +3,43 @@
 ## openpyxl
 
 > doc: https://openpyxl.readthedocs.io/en/latest/
+
+### openpyxl sample demo
+```python
+#创建一个工作薄对象,也就是创建一个excel文档
+wb = Workbook()
+
+#指定当前显示（活动）的sheet对象
+ws = wb.active
+
+ws.append([1, 2, 3])
+wb.save("sample.xlsx")
+
+wb = load_workbook('1.xlsx')
+# 根据sheet名称选取
+ws = wb['sheet']
+
+for row in ws.rows:
+    for cell in row:
+        print(cell.value)
+```
+
+### python.str is,==
+
+```python
+s1 = "hello"
+s2 = "hellotest"
+s1 is s2[0:5] # False
+s1 == s2[0:5] # True
+```
+
+### python.shutil
+
+```python
+try:
+    shutil.move(row[0].value,row[1].value)
+except FileNotFoundError as e:
+    print("FileNotFoundError")
+```
+
+
--- xlsx/core.py
+++ xlsx/core.py
@@ -2,6 +2,7 @@
 
 import openpyxl
 import os
+import shutil
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,10 +11,19 @@ def _get_pic_uri():
 
 def _get_pic_files():
     path = _get_pic_uri()
-    print(path)
     files = os.listdir(path)
+    pic_files = []
     for i in range(len(files)):
-        print(files[i])
+        if _is_pic_file(files[i],path):
+            pic_files.append(files[i])
+    return pic_files
+
+def _is_pic_file(filename,path):
+    full_filename = os.path.join(path,filename)
+    if os.path.isfile(full_filename) and 'Pictures' == filename[0:8]:
+        return True
+    else:
+        return False
 
 def _get_full_filename(filename,dir):
     dir_name = os.path.join(os.getcwd(),dir)
@@ -26,14 +36,47 @@ def _get_full_filename(filename,dir):
             f.close()
     return full_name
 
+def _get_content():
+    content = []
+
+    old_path = _get_pic_uri()
+    new_path = os.path.join(old_path,"Pictures")
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+
+    pic_files = _get_pic_files()
+    for pic in pic_files:
+        pic_content = [os.path.join(old_path,pic),os.path.join(new_path,pic.replace('Pictures',''))]
+        content.append(pic_content)
+
+    return content
+
 def build_xlsx():
     xlsx_name = _get_full_filename("test.xlsx","docs")
     workbook = openpyxl.Workbook()
+    ws = workbook.active
+
+    pics = _get_content()
+    for pic in pics:
+        ws.append(pic)
     workbook.save(xlsx_name)
 
+def files_move():
+    xlsx_name = _get_full_filename("test.xlsx","docs")
+    workbook = openpyxl.load_workbook(xlsx_name,read_only=True)
+    ws = workbook.active
+
+    rows = ws.rows
+    for row in rows:
+        print(row[0].value,"->",row[1].value)
+        try:
+            shutil.move(row[0].value,row[1].value)
+        except FileNotFoundError as e:
+            print("FileNotFoundError")
+
 def main():
-    # _get_pic_files()
-    build_xlsx()
+    # build_xlsx()
+    files_move()
 
 if __name__ == '__main__':
     main()
