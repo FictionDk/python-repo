ee43b082f3beffdb15d59cabb09f4e93f8754eab
hek <ficito.d2k@gmail.com>
2020-03-05 23:51:49 +0800

    feat: Build item details;#200305

--- scrapy-note/blade/spiders/douban.py
+++ scrapy-note/blade/spiders/douban.py
@@ -6,11 +6,8 @@ from blade.items import DouBanItem
 
 class DoubanSpider(scrapy.Spider):
     name = "douban"
-    allowed_domains = ["baidu.com"]
+    allowed_domains = ["www.douban.com"]
     url = "https://www.douban.com/group/futianzufang/discussion?start="
-    # url = "http://top.baidu.com/?fr=mhd_card"
-    # url = "https://www.douban.com/gallery/topic/131026/?from=hot_topic_anony_sns"
-    # url = 'http://quotes.toscrape.com/'
     page = 0
 
     def start_requests(self):
@@ -21,28 +18,49 @@ class DoubanSpider(scrapy.Spider):
 
     def parse(self, response):
         self.logger.info('Douban page: %s', response.url)
-        result_tables = response.xpath('//table')
-        tr_rows = None
-        if(result_tables is not None and len(result_tables) > 1):
-            tr_rows = result_tables[1].xpath('tr')
-        for tr_row in tr_rows:
-            item = self._build_douban_item(tr_row)
-            yield item
-        time.sleep(4)
+        hrefs = response.css('.olt .title a::attr(href)').extract()
+        for href in hrefs:
+            if href:
+                yield scrapy.Request(url=href, callback=self.parse_detail)
+            time.sleep(4)
         self.page += 25
         print("URL: %s%s" % (self.url,self.page))
         if self.page <= 100:
             yield scrapy.Request(url=self.url + str(self.page), callback=self.parse)
 
-    def _build_douban_item(self,tr_row):
-        td_rows = tr_row.xpath('td')
+    def parse_detail(self, response):
         item = DouBanItem()
-        item['article_url'] = td_rows[0].xpath('a/@href').get()
-        item['article_title'] = td_rows[0].xpath('a/@title').get()
-        item['author_url'] = td_rows[1].xpath('a/@href').get()
-        item['author_name'] = td_rows[1].xpath('a/text()').get()
-        item['createtime'] = td_rows[3].xpath('text()').get()
-        return item
+        topic_content = response.css('#topic-content')
+        item['article_url'] = response.url
+        item['article_title'] = self._content_format(topic_content.css('.infobox td::text').extract())
+        item['article_content'] = self._content_format(topic_content.css('.topic-richtext p::text').extract())
+        item['article_imgs'] = topic_content.css('.topic-richtext img::attr(src)').extract()
+        item['create_time'] = topic_content.css('h3 .color-green::text').get()
+        author = topic_content.css('.user-face')
+        item['author_url'] = author.css('a::attr(href)').get()
+        item['author_name'] = author.css('img::attr(alt)').get()
+        item['author_img'] = author.css('img::attr(src)').get()
+        comment_slector = response.css('#comments li')
+        comment_list = []
+        for comment_row in comment_slector:
+            comment_list.append(self._build_comment(comment_row))
+        item['comments'] = comment_list
+        yield item
+
+    def _build_comment(self, comment_row):
+        name = comment_row.css('img::attr(alt)').get()
+        img = comment_row.css('img::attr(src)').get()
+        url = comment_row.css('.user-face a::attr(href)').get()
+        content = comment_row.css('.reply-content::text').extract()
+        return name + "|" + img + "|" + url + "|" + self._content_format(content)
+
+    # 将list中的内容进行拼接，形成字符串
+    def _content_format(self,in_list):
+        result = ''
+        for list_item in in_list:
+            result = result + list_item
+
+        return result
 
     def _get_full_filename(self,filename,dir):
         dir_name = os.path.join(os.getcwd(),dir)
