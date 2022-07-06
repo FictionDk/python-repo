51af7eb213d324892fc5c6c37134fbb6f2bc1cd0
fictio <ficito.d2k@gmail.com>
2022-07-06 12:49:17 +0800

    feat: comMockUpdated;#220706

--- base-api-test/com_mock.py
+++ base-api-test/com_mock.py
@@ -9,7 +9,7 @@ def set_com_value(comNumber, value):
     flag = True
     while flag:
         try:
-            ser = serial.Serial(comNumber, baudrate=9600, parity=serial.PARITY_ODD, bytesize=serial.EIGHTBITS, timeout=0.5)
+            ser = serial.Serial(comNumber, baudrate=9600, parity=serial.PARITY_ODD, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, timeout=0.5)
             print(ser)
             r = ser.write((value).encode('utf-8'))
             print("打开串口{0},写入结果{1}".format(ser.name,r))
@@ -18,9 +18,23 @@ def set_com_value(comNumber, value):
             print("打开串口{0},失败".format(comNumber))
             time.sleep(0.5)
 
-def set_1Com():
-    set_com_value('COM6','B21C2A31')
+def setPlateVal():
+    set_com_value('COM8','2207041171\r\n')
+    set_com_value('COM6','2207041172\r\n')
+
+def set_plc_val():
+    set_com_value('COM2','%01$RD010001000000000001000100000016\r\n')
+
+def setLabelVal():
+    pre_val = '220714B000'
+    for i in range(3):
+        set_com_value('COM10',pre_val+str(i)+"\r\n")
+        # set_com_value('COM12',pre_val+str(i*2)+"\r\n")
+        time.sleep(1)
+
 
 if __name__ == "__main__":
-    set_1Com()
+    setPlateVal()
     time.sleep(3)
+    setLabelVal()
+    set_plc_val()
